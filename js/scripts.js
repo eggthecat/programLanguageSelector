fc1a45ff528213ef80b1a844f2f04b4648c5863b
Dominic Montelongo <montelongodominic@gmail.com>
2019-08-16 11:42:36 -0700

    Add basic layout for input

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#formOne").submit(event(){
+    var 1 = $("#1");
+    var 2 = $("#2");
+    var 3 = $("#3");
+    var 4 = $("#4");
+    var 5 = $("#5");
+
+    event.preventDefault()
+  });
+});
