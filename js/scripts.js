3d960ca0275b09b6373ba48d9d31acfe8aede6ac
Dominic Montelongo <montelongodominic@gmail.com>
2019-08-16 15:11:07 -0700

    Add varibale place holders

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
   $("#formOne").submit(event(){
-    var 1 = $("#1");
-    var 2 = $("#2");
-    var 3 = $("#3");
-    var 4 = $("#4");
-    var 5 = $("#5");
-
     event.preventDefault()
+
+    var 1 = ($("#1").val());
+    var 2 = ($("#2").val());
+    var 3 = ($("#3").val());
+    var 4 = ($("#4").val());
+    var 5 = ($("#5").val());
+
   });
 });
