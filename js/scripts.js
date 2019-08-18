887c471b28ecfb7af1bcd75c5cd07c5a03303f6b
Dominic Montelongo <montelongodominic@gmail.com>
2019-08-17 20:05:36 -0700

    Add if/else framework to submit function

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,27 @@
 function reload(){
   $(".field").val(0);
   $(".userResult").hide();
-}
+};
 
 $(document).ready(function(){
-  $("#formOne").submit(event(){
+  $("#formOne").submit(event){
     event.preventDefault()
     $(".userResult").hide()
 
-
-
     var interest = parseInt($("#interest").val());
-    var program = text($("#program").val());
-    var time = text($("#time").val());
-    var internship = text($("#internship").val());
+    var program = parseInt($("#program").val());
+    var time = parseInt($("#time").val());
+    var internship = parseInt($("#internship").val());
     var location = parseInt($("#location").val());
 
     var interestScore = interest;
-    var locationScore = location;
-
     var programScore = program;
     var timeScore = time;
-    var intershipScore = time;
+    var internshipScore = internship;
+    var locationScore = location;
+
+    if (interestScore > 1) {
+      $("")
+    }
   });
 });
