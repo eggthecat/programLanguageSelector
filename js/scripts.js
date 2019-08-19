78ae44adbe85f3eb3590075f2317b2d4d9127b21
Dominic Montelongo <montelongodominic@gmail.com>
2019-08-18 21:48:35 -0700

    Add if/else for 'var Program'

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,42 @@ $(document).ready(function(){
     var internship = parseInt($("#internship").val());
     var location = parseInt($("#location").val());
 
-    if (interest = 0) {
+    if (interest === 0) {
       alert ("Please indicate whether you are interested in becoming a software developer!");
-      $("#.userResult").hide();
-    } else if (interest = 1) {
+      $(".userResult").hide();
+    } else if (interest === 1) {
       alert ("This quiz is for individuals interested in software development.");
       $("#.userResult").hide();
-    } else (interest = 2) {
+    } else if (interest === 2) {
+      $("#congratulations").show();
+    };
+
+    if (program === 0) {
+      alert ("Please indicate what platform interests you.");
+      $(".userResult").hide();
+    } else if (program === 1) {
+      $("#cSharp").show();
+      $("#go").hide();
+      $("#python").hide();
+      $("#swift").hide();
+    } else if (program === 2) {
+      $("#cSharp").hide();
+      $("#go").show();
+      $("#python").hide();
+      $("#swift").hide();
+    } else if (program === 3) {
+      $("#cSharp").hide();
+      $("#go").hide();
+      $("#python").show();
+      $("#swift").hide();
+    } else if (program === 4) {
+      $("#cSharp").hide();
+      $("#go").hide();
+      $("#python").hide();
+      $("#swift").show();
     }
+  });
+});
 
     // var interestScore = interest;
     // var programScore = program;
@@ -40,6 +68,3 @@ $(document).ready(function(){
     //   }
     // }
     //
-
-  });
-});
